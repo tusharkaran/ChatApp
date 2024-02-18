9e19528c8cd372a51d67f7c87dd191c3e25f93c5
Karan Tushar <karantushar@karans-air.home>
2024-02-18 08:50:47 -0500

    new push changes

--- backend/server.js
+++ backend/server.js
@@ -7,50 +7,35 @@ const userRoutes = require('./routes/userRoutes')
 const {notFound,errorHandler} = require('../backend/middleware/errorMidleware')
 const chatRoutes = require('./routes/chatRoutes')
 const messageRoutes = require('./routes/messageRoutes')
-const path = require("path");
+
  connectDB();
 dotenv.config()
 app.use(express.json());
-
+// app.get("/",(req,res)=>{
+//     res.send("API is Running successfly ");
+// })
+app.use(express.static('./frontend/build'));
 
 app.use('/api/user',userRoutes)
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "frontend", "build",     
+   "index.html"));
+});
 app.use(notFound);
 app.use(errorHandler)
 
-// --------------------------deployment------------------------------
-
-const __dirname1 = path.resolve();
-console.log(__dirname1);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
-// --------------------------deployment------------------------------
-
-
-  const PORT= process.env.PORT || 8080
+const PORT= process.env.PORT || 8080
 const server = app.listen(PORT,console.log("Server Started on PORT ",PORT));
-
-
 
 const io = require("socket.io")(server,{
     pingTimeout:6000,
     cors:{
-        origin: "https://talktome.azurewebsites.net/",
+        origin: "http://localhost:3000",
     },
 });
-
 io.on("connection", (socket) =>{
     console.log("cpnnected to socket.io");
     socket.on("setup" , (userData) =>{
@@ -87,10 +72,4 @@ io.on("connection", (socket) =>{
     console.log("USER DISCONNECTED");
     socket.leave(userData._id);
   });
-
-
 });
-
-
-
-
