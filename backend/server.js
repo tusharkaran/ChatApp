ee214ace943f45e02320f33139baf61ba9544f2d
Karan Tushar <karantushar@Karans-MacBook-Air.local>
2024-02-26 14:30:54 -0500

    new changes

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,9 @@ const path = require("path");
  connectDB();
 dotenv.config()
 app.use(express.json());
-app.get("/",(req,res)=>{
-    res.send("API is Running successfly ");
-})
+// app.get("/",(req,res)=>{
+//     res.send("API is Running successfly ");
+// })
 const __dirname1 = path.resolve();
 app.use(express.static(path.join(__dirname1, "/backend/frontend/build")));
 
